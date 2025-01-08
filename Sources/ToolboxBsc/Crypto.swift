2d5e701712446ab801710ce0328f34895d8c063c
ChenLin Wang <wangchenlin2001@icloud.com>
2025-01-08 12:40:57 +0800

    V0.1.2.1 备份

--- Sources/ToolboxBsc/Crypto.swift
+++ Sources/ToolboxBsc/Crypto.swift
@@ -1,10 +1,38 @@
 import Crypto
 import Foundation
 
-class Crypto {
-
+enum EncryptionError: Error {
+    case conversionFailed
+    case unsupportedType
 }
 
-class SecretKey {
+class Crypto {
+    func symmetricEncrypt<DataType: DataProtocol>(_ data: DataType, key: SymmetricKey) throws -> DataType {
+        let sealedBox = try AES.GCM.seal(data, using: key)
+        let ciphertext = sealedBox.ciphertext
+
+        if DataType.self == String.self {
+            // Convert ciphertext to String if DataType is String
+            guard let stringResult = String(data: ciphertext, encoding: .utf8) as? DataType else {
+                throw EncryptionError.conversionFailed
+            }
+            return stringResult
+        } else if DataType.self == Data.self {
+            // Return as Data if DataType is Data
+            guard let dataResult = ciphertext as? DataType else {
+                throw EncryptionError.conversionFailed
+            }
+            return dataResult
+        } else if DataType.self == [UInt8].self {
+            // Convert Data to [UInt8] if DataType is [UInt8]
+            guard let arrayResult = Array(ciphertext) as? DataType else {
+                throw EncryptionError.conversionFailed
+            }
+            return arrayResult
+        } else {
+            throw EncryptionError.unsupportedType
+        }
+    }
+
 
 }
