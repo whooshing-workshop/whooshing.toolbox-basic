021537d363b37321fecbac8b882c3815165b74ec
ChenLin Wang <wangchenlin2001@icloud.com>
2025-03-31 12:28:59 +0800

    V1.1.11.25 update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         .library( name: "DataConvertable", targets: ["DataConvertable"] ),
         .library( name: "Cryptos", targets: ["Cryptos"] ),
         .library( name: "PgSQL", targets: ["PgSQL"] ),
+        .library( name: "Client", targets: ["WhooshingClient"] ),
     ],
     dependencies: [
         .package(url: "https://github.com/SJJC-Team/whooshing-vapor.git", branch: "main"),
