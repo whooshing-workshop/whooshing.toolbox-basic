fb38160dc150d3663d3e62d525f6d8a792ac1142
ChenLin Wang <wangchenlin2001@icloud.com>
2026-06-10 15:06:40 +0800

    V1.5.2.2 增加 bridge 函数以替代 makeResultWithTask。后者可能引发 bug

---
+++ Sources/NIOAdvanced/EventLoop+Bridge.swift
@@ -0,0 +1,41 @@
+import NIOCore
+
+public extension EventLoop {
+    // 使用原生的 makeResultWithTask 或 makeFutureWithTask 可能会出现非预期的 bug
+    // 可使用 bridge 替换
+    func bridge<R: Sendable, E>(
+        throws: E.Type = E.self,
+        _ action: @escaping @Sendable () async throws(E) -> R
+    ) -> EventLoopResult<R, E> {
+        let target = self.makeTarget(of: R.self, throws: E.self)
+        
+        Task {
+            do {
+                let r = try await action()
+                target.succeed(r)
+            } catch {
+                target.fail(error as! E)
+            }
+        }
+        
+        return target.futureResult
+    }
+
+    func bridge<R: Sendable>(
+        _ action: @escaping @Sendable () async throws -> R
+    ) -> EventLoopFuture<R> {
+        let promise = self.makePromise(of: R.self)
+        
+        Task {
+            do {
+                let r = try await action()
+                promise.succeed(r)
+            } catch {
+                promise.fail(error)
+            }
+        }
+        
+        return promise.futureResult
+    }
+
+}
